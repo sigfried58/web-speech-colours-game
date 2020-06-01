5a1c601854dea0ade75cc44ca5e59d8315a81a11
jimmy ibanez <jimmy.ibanez@outlook.com>
2020-06-01 19:35:20 -0400

    feat(Game) : SpeechRecognition was implemented

---
+++ handlers.js
@@ -0,0 +1,26 @@
+import { isValidColor } from './colors';
+
+function logWords(results) {
+  console.log(results[results.length - 1][0].transcript);
+}
+
+export function handleResult({ results }) {
+  logWords(results);
+  const words = results[results.length - 1][0].transcript;
+  console.log(words);
+
+  // lowercase everyrhing
+  let color = words.toLowerCase();
+  // strip any spaces out
+  color = color.replace(/\s/g, '');
+  // check if it is a valid colour
+  if (!isValidColor(color)) return; // thats all folks
+  // if it is, then show the UI for that
+  const colorSpan = document.querySelector(`.${color}`);
+  colorSpan.classList.add('got');
+  console.log(colorSpan);
+  console.log('This is valid color!');
+  console.log(color);
+  // change the background color
+  document.body.style.background = color;
+}
